35053714986c6e5a5f298eed10014edcb20a45cd
Jarett <jarettr@opspl.com>
2020-03-18 18:25:15 +0530

    Added changes to user UI

--- src/app/admin/dashboard/admin-dashboard.jsx
+++ src/app/admin/dashboard/admin-dashboard.jsx
@@ -1,65 +1,56 @@
-import React, {Component} from 'react';
-import { Form, Input, InputNumber, Button, notification } from 'antd';
+import React, { Component } from "react";
+import { PageHeader, Row, Table, Popconfirm, Button, Modal, notification } from 'antd';
+import 'antd/dist/antd.css';
 import axios from "axios";
 import {Route, Link} from 'react-router-dom'
-import 'antd/dist/antd.css';
-
-class AdminDashboard extends Component {
-
-  constructor(props) {
-    super(props);
-      this.state = { 
-        data : []
-     }
-  }
-
-  
-  render() { 
-
-    const layout = {
-      labelCol: { span: 8 },
-      wrapperCol: { span: 16 },
-    };
-    
-    const validateMessages = {
-      required: 'This field is required!',
-      types: {
-        email: 'Not a validate email!',
-        number: 'Not a validate number!',
-      },
-      number: {
-        range: 'Must be between ${min} and ${max}',
-      },
-    };
+import { fetchApi } from "../../../services/api";
 
 
-    const onFinish = values => {
-      console.log("values",values);
-      let resStatus = 0;
-      let url = `http://127.0.0.1:5000/api/v1/user-types/`;
-      axios({
-        method: "POST",
-        url: url,
-        headers: {
-          Accept: "application/json",
-          //"X-API-KEY": getCookie(COOKIE.ID_TOKEN),
-          "Content-Type": "application/json"
-          //location: getCookie(COOKIE.FARM)
-        },
-        data: JSON.stringify(values)
-      })
+class List extends React.Component {
+  state = {
+    data: [],
+    pagination: {},
+    loading: false,
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleDelete = (key, e) => {
+    e.preventDefault();
+    const data = this.state.data.filter(item => item.publicId !== key);
+    this.setState({data: data});
+    let resStatus = 0; 
+    const values = {publicId: key}
+    fetchApi(`/users/`, "delete", values) 
         .then(res => {
           resStatus = res.status;
-          console.log(resStatus)
-          console.log("Response data", res);
+          console.log("res123", resStatus)  
           switch (resStatus) {
             case 201:
               notification.success({
                 message: "Success",
                 duration: 3,
-                description: "Data saved Successfully"
+                description: "Deleted Successfully"
               });
-              //console.log("Response data", res);
+              console.log("Response data", res);
               break;
             case 400:
               notification.error({
@@ -85,44 +76,114 @@ class AdminDashboard extends Component {
       .catch(err => {
         console.error(err);
       });   
-    };
+  };
 
-    return ( 
-      <div> 
-         
-        <Link to="/">
+  handleTableChange = (pagination, filters, sorter) => {
+    const pager = { ...this.state.pagination };
+    pager.current = pagination.current;
+    this.setState({
+      pagination: pager,
+    });
+    this.fetch({
+      results: pagination.pageSize,
+      page: pagination.current,
+      sortField: sorter.field,
+      sortOrder: sorter.order,
+      ...filters,
+    });
+  };
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  fetch = () => {
+    this.setState({ loading: true });
+    fetchApi(`/users/`, "get").then(res => {
+        this.setState({ 
+            loading: false,
+            data: res.data
+         });
+      })
+  };
+
+  render()
+  {
+
+    const columns = [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          sorter: true,
+          width: '20%',
+          key: 'publicId',
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'publicId',
+        },
+        {
+          title: 'Age',
+          dataIndex: 'age',
+          key: 'publicId',
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          dataIndex: 'publicId',
+          key: 'publicId',
+          render: (text, record) => (
+            <span>
+              <div>
+              {/* <Link to="/edit" >
+                  <a type="link" block>
+                      Edit
+                  </a>
+              </Link> */}
+            
+            <Link
+                to={{
+                  pathname: "/edit",
+                  state: {
+                  edit_records: record
+                  }
+                  }}
+                  //onClick={() => this.setPackId(id)}
+                  >
+                Edit
+            </Link>
+            </div>
+              
+              <Popconfirm title="Sure to delete?" onConfirm={(e) => this.handleDelete(record.publicId, e)}>
+                <a>Delete</a>
+              </Popconfirm>
+              {/* <a>Delete</a> */}
+            </span>
+          ),
+        }     
+      ];
+      
+    console.log("updated data", this.state.data)
+    let {data}=this.state;
+    return (
+    <div>
+        <Link to="/add">
         <Button type="link" block>
-            Back
+            Add   
         </Button>
         </Link>
-        <div>
-       <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-       <Form.Item name={['name']} label="Name" rules={[{ required: true }]}>
-       <Input />
-     </Form.Item>
-     <Form.Item name={['email']} label="Email" rules={[{ type: 'email' }]}>
-       <Input />
-     </Form.Item>
-     <Form.Item name={['age']} label="Age" rules={[{ type: 'number', min: 18, max: 99 }]}>
-       <InputNumber />
-     </Form.Item>
-     <Form.Item name={['website']} label="Website">
-       <Input />
-     </Form.Item>
-     <Form.Item name={['introduction']} label="Introduction">
-       <Input.TextArea />
-     </Form.Item>
-     <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-       <Button type="primary" htmlType="submit">
-         Save
-       </Button>
-     </Form.Item>
-      </Form>
-      </div>
+      <Table
+        columns={columns}
+        rowKey={record => record.login}
+        dataSource={data}
+        pagination={this.state.pagination}
+        loading={this.state.loading}
+        onChange={this.handleTableChange}
+      />
     </div>
-     );
+    );
   }
 }
- 
-export default AdminDashboard;
 
+export default List
